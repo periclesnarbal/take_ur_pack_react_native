54ab8cdd4062e4de20bb03d6dedf337f008b0c76
Péricles Narbal <periclesnarbal@gmail.com>
2024-03-11 14:40:49 -0300

    change expo-app-loading for expo-splah-screen

--- src/components/StyledText.tsx
+++ src/components/StyledText.tsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Text, TextStyle } from "react-native";
 
 import { useFonts } from "expo-font";
-import AppLoading from 'expo-app-loading';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 const StyledText = ({ title, style }: { title: string; style?: TextStyle }) => {
-  const [fontsLoaded] = useFonts({
+  const [fontsLoaded, fontError] = useFonts({
     'SFProDisplay-Regular': require('../../assets/fonts/SFProDisplay/SF-Pro-Display-Regular.otf'),
   });
-    
-  if (!fontsLoaded) return <AppLoading />;
 
-  return <Text style={style}>{title}</Text>;
+  const onLayoutRootView = useCallback(async () => {
+    if(fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  return <Text style={style} onLayout={onLayoutRootView}>{title}</Text>;
 };
 
 export default StyledText;
